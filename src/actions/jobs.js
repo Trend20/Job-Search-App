d31c2d830ff777a080254fcd521c0358f3b381f0
Trend20 <enockomondi305@gmail.com>
2021-06-21 14:08:30 +0300

    adding the search component

---
+++ src/actions/jobs.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+import moment from 'moment';
+import { BASE_API_URL } from '../utils/constants';
+import { setErrors } from './errors';
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Search from './Search';
 
 const HomePage = () => {
-	return <div className="container">HomePage</div>;
+	return (
+		<div className="container">
+			<Header />
+			<Search />
+		</div>
+	);
 };
 
 export default HomePage;
